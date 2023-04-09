6374105d64dc7c8cc9de49e15a45f005d054fe84
Ekmech <emir.can2@hotmail.com>
2023-04-10 01:55:40 +0300

    Update EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -87,7 +87,7 @@ public class Enemy : MonoBehaviour
     {
         // Düþmanýn saldýrý animasyonunu çalýþtýrýr ve oyuncuya hasar verir
         // Burada saldýrý animasyonu ve saldýrý sýrasýnda alýnacak aksiyonlar yazýlabilir
-        player.TakeDamage(damageAmount);
+        
     }
 }
         
